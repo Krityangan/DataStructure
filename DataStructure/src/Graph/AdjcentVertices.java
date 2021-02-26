f32fe5e11f363188a9b4b27ebcd632d89c5bc435
krityangan Rahul <krityangan@gmail.com>
2021-02-26 21:10:57 +0530

    Graph And Dp Update

---
+++ DataStructure/src/Graph/AdjcentVertices.java
@@ -0,0 +1,66 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdjcentVertices {
+	
+	static class Edge{
+		int src;
+		int nbr;
+		int w;
+		
+		public Edge(int src, int nbr, int w) {
+			super();
+			this.src = src;
+			this.nbr = nbr;
+			this.w = w;
+		}
+
+		@Override
+		public String toString() {
+			return "Edge [src=" + src + ", nbr=" + nbr + ", w=" + w + "]";
+		}
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		int vces=7;
+		List<Edge>[] graph = new ArrayList[7];
+		for(int i=0;i<vces;i++)
+		{
+			graph[i]= new ArrayList<Edge>();
+		}
+		
+		graph[0].add(new Edge(0, 3, 40));
+		graph[0].add(new Edge(0, 1, 10));
+		
+		graph[1].add(new Edge(1, 0, 10));
+		graph[1].add(new Edge(1, 2, 10));
+		
+		graph[2].add(new Edge(2, 3, 10));
+		graph[2].add(new Edge(2, 1, 10));
+		
+		graph[3].add(new Edge(3, 0, 40));
+		graph[3].add(new Edge(3, 2, 10));
+		graph[3].add(new Edge(3, 4, 2));
+		
+		graph[4].add(new Edge(4, 3, 2));
+		graph[4].add(new Edge(4, 5,3));
+		graph[4].add(new Edge(4, 6, 3));
+		
+		graph[5].add(new Edge(5, 4, 3));
+		graph[5].add(new Edge(5, 6, 3));
+		
+		graph[6].add(new Edge(6, 5, 3));
+		graph[6].add(new Edge(6, 4, 8));
+		
+		
+			
+
+	}
+
+}
---
+++ DataStructure/src/com/example/dynamicPrograming/SubArray.java
@@ -0,0 +1,24 @@
+package com.example.dynamicPrograming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubArray {
+
+	public static void main(String[] args)
+	{
+		int arr[]= {1,2,3,4,5,6,7,8};
+		int k=2;
+		subArray(arr,0,k,0);
+	}
+
+	private static void subArray(int[] arr, int cInd,int seq,int cSeq) {
+		
+			//if(cInd >= arr.length)
+				//printArray()
+		
+		
+	}
+	
+	
+}
